cb1f36cd2589697cab4e462224d22a6d3d0f05ef
Alexey <aa.gorschenko@yandex.ru>
2022-11-21 11:52:47 +0300

    Server. ExeptionFilter has been fixed

--- server/src/errors/exeption.filter.ts
+++ server/src/errors/exeption.filter.ts
@@ -15,10 +15,18 @@ export class ExeptionFilter implements IExeptionFilter {
   catch(err: Error | HTTPError, req: Request, res: Response, next: NextFunction): void {
     if (err instanceof HTTPError) {
       this.logger.error(`[${err.context}] Ошибка ${err.statusCode}: ${err.message}`)
-      res.status(err.statusCode).send({ err: err.message })
+      res.status(err.statusCode).send({
+        ok: true,
+        err: err.message,
+        payload: null,
+      })
     } else {
       this.logger.error(`${err.message}`)
-      res.status(500).send({ err: err.message })
+      res.status(500).send({
+        ok: true,
+        err: err.message,
+        payload: null,
+      })
     }
   }
 }
